b06d41264b8c296bd7cfd6b27f546e2459c00bf2
triciaglassborow <22212293@bucks.ac.uk>
2024-01-08 14:38:33 +0000

    displayfloorchart comments

--- OnlineTicketSystem/FloorChart.h
+++ OnlineTicketSystem/FloorChart.h
@@ -10,12 +10,14 @@ class FloorChart
 public:
 	FloorChart();
 	void DisplayFloorChart();
+	void SelectSeat();
 
 protected:
 	int floor[3][5];
-	int available = 'A';
-	int held =  'H';
-	int unavailble = 'X';
+	char available = 'A';
+	char held =  'H';
+	char unavailble = 'X';
+	string seat;
 };
 
 FloorChart::FloorChart()
@@ -32,20 +34,24 @@ void FloorChart::DisplayFloorChart()
 	for (int r = 0; r < 3; ++r)
 	{
 		if (r==0)
-			cout << "     A     B     C     D     E  \n" <<
+			cout << "     A     B     C     D     E  \n" << // coulmn lables
 					"   _____________________________" << 
 			      "\n  |     |     |     |     |     |\n"; // adding a top to first row
-		cout << r+1 <<" |";
+		cout << r+1 <<" |"; // row lables
 		for (int c = 0; c < 5; ++c)
 		{
-			cout << setw(3) << static_cast<char>(floor[r][c]);
-
-			if (c != 2||3||4)
-				cout << "  |";
+			cout << setw(3) << static_cast<char>(floor[r][c]); // adding the seat availablity
+			cout << "  |";
 		}
 		if (r !=2) 
 			cout << "\n  |_____|_____|_____|_____|_____|" << "\n  |     |     |     |     |     |\n"; // adding the row line to all of them then adding the beggining of the next box
 		if (r == 2)
 			cout << "\n  |_____|_____|_____|_____|_____|"; // adding a bottom to the final row
 	}
+}
+
+void FloorChart::SelectSeat()
+{
+	cout << "Enter a seat you would like, eg 2B: ";
+	cin >> seat;
 }
